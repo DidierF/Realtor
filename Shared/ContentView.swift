176311fab290de5b0ad99f7af153d71ec8c157d6
DidierF <didier.fuentes@gmail.com>
2022-07-13 21:17:01 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Didier Fuentes on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RealtorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtorApp.swift
+//  Shared
+//
+//  Created by Didier Fuentes on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct RealtorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
